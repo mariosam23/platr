b6c3804409284b4397bf0bcdd73096eaa24911c7
Sâmpetru D. Mario <119307188+mariosam23@users.noreply.github.com>
2026-03-29 12:08:52 +0300

    Protected routes

--- platr-frontend/src/App.tsx
+++ platr-frontend/src/App.tsx
@@ -1,36 +1,38 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { PlatrRoutes } from './application/routes';
 import NavBar from './components/NavBar';
+import ProtectedRoute from './components/ProtectedRoute';
 import { Home } from './pages/Home';
 import { Login } from './pages/Login';
-import './styles/App.css';
-import { Register } from './pages/Register';
+import { Feedback } from './pages/Feedback';
 import { MealPlanDetail } from './pages/MealPlanDetail';
 import { MealPlans } from './pages/MealPlans';
-import { Feedback } from './pages/Feedback';
 import { RecipeDetail } from './pages/RecipeDetail';
 import { Recipes } from './pages/Recipes';
-import ProtectedRoute from './components/ProtectedRoute';
+import { Register } from './pages/Register';
+import './styles/App.css';
 
 function App() {
-  return (
-    <BrowserRouter>
-      <NavBar />
-      <div className="container">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/recipes/:id" element={<RecipeDetail />} />
-          <Route element={<ProtectedRoute />}>
-            <Route path="/recipes" element={<Recipes />} />
-            <Route path="/mealplans" element={<MealPlans />} />
-            <Route path="/mealplans/:id" element={<MealPlanDetail />} />
-            <Route path="/feedback" element={<Feedback />} />
-          </Route>
-        </Routes>
-      </div>
-    </BrowserRouter>
-  )
+    return (
+        <BrowserRouter>
+            <NavBar />
+            <div className="container">
+                <Routes>
+                    <Route path={PlatrRoutes.Home} element={<Home />} />
+                    <Route path={PlatrRoutes.Login} element={<Login />} />
+                    <Route path={PlatrRoutes.Register} element={<Register />} />
+                    <Route path={PlatrRoutes.Feedback} element={<Feedback />} />
+
+                    <Route element={<ProtectedRoute />}>
+                        <Route path={PlatrRoutes.Recipes} element={<Recipes />} />
+                        <Route path={PlatrRoutes.RecipeDetail} element={<RecipeDetail />} />
+                        <Route path={PlatrRoutes.MealPlans} element={<MealPlans />} />
+                        <Route path={PlatrRoutes.MealPlanDetail} element={<MealPlanDetail />} />
+                    </Route>
+                </Routes>
+            </div>
+        </BrowserRouter>
+    );
 }
 
 export default App;
